00ae792a50a7c9cbdce172ed9749e05c0957ff46
wassim <wassimlecheb@gmail.com>
2025-05-18 11:46:12 +0100

    hachak

--- frontend/src/components/HelpPage.jsx
+++ frontend/src/components/HelpPage.jsx
@@ -187,25 +187,7 @@ const HelpPage = () => {
 
   return (
     <div className="help-page">
-      <header>
-        <nav className="navbar">
-          <div className="logo">E-med</div>
-          <ul className="nav-links">
-            <li><Link to="/LandingPage">Home</Link></li>
-            <li><Link to="/ScanPage">Scan</Link></li>
-            <li><Link to="/About">About</Link></li>
-            <li><Link to="/HelpPage">Help & Support</Link></li>
-          </ul>
-          <div className="nav-buttons">
-            <button className="login-btn">Log in</button>
-            <button className="signup-btn">Sign up</button>
-            <span className="settings-icon" onClick={toggleSidebar}>
-              <FontAwesomeIcon icon={faBars} style={{ color: "#1e57b8" }} />
-            </span>
-          </div>
-          <div className="menu-toggle" onClick={toggleMenu}>☰</div>
-        </nav>
-      </header>
+     
       
       <aside className={`sidebar ${!sidebarVisible ? 'hidden' : ''}`} id="sidebar">
         <div className="sidebar-section">
@@ -302,48 +284,6 @@ const HelpPage = () => {
         </div>
       </section>
 
-      <footer className="footer">
-        <div className="container">
-          <div className="row">
-            <div className="footer-col">
-              <h4>Company</h4>
-              <ul>
-                <li><a href="/">About Us</a></li>
-                <li><a href="/">Our Services</a></li>
-                <li><a href="/">Privacy Policy</a></li>
-                <li><a href="/">Affiliate Program</a></li>
-              </ul>
-            </div>
-            <div className="footer-col">
-              <h4>Get Help</h4>
-              <ul>
-                <li><a href="/">FAQs</a></li>
-                <li><a href="/">Payment options</a></li>
-              </ul>
-            </div>
-            
-            <div className="footer-col">
-              <h4>Follow Us</h4>
-              <div className="social-links">
-                <a href="/"><i className="fab fa-facebook-f"></i></a>
-                <a href="/"><i className="fab fa-twitter"></i></a>
-                <a href="/"><i className="fab fa-instagram"></i></a>
-                <a href="/"><i className="fab fa-linkedin-in"></i></a>
-              </div>
-            </div>
-          </div>
-          <br />
-          <br />
-          <br />
-          <hr />
-          <br />
-          <div className="row">
-            <div className="copyright">
-              <p>&copy; {new Date().getFullYear()} SCANAI. All rights reserved.</p>
-            </div>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 };
